5761eb394a8f8348256fe2be373b6b8b07030e9f
Marta Domagała <mahomegol@gmail.com>
2025-09-15 12:22:53 +0200

    Create logging_setup.py

---
+++ match_scraper/logging_setup.py
@@ -0,0 +1,26 @@
+import logging
+from logging.config import dictConfig
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "format": "%(asctime)s [%(levelnames)s] %(name)s: %(message)s",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+            "level": "INFO",
+        },
+    },
+    "root": {
+        "level": "INFO",
+        "handlers": ["console"],
+    },
+}
+
+def setup_logging() -> None:
+    dictConfig(LOGGING_CONFIG)
